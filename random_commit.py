5fd6df0a0759039689ba7476d0f1760e128cb573
Anonymob <anmolsharma1303@gmail.com>
2024-12-03 11:13:27 +0530

    for

--- random_commit.py
+++ random_commit.py
@@ -1,3 +1,6 @@
 print("hi this is  agit commit test to check if evertyhing is working or not...")
 print("well it looks good to go damn")
 # just a random comment
+
+for i in range(1,10):
+    print (i)
